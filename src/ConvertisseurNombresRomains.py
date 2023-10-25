f04e0bc1ef84c960a4811cc1c14bcc06d163e8f8
Enzo Sandré <atelier@enzosandre.fr>
2023-10-25 14:34:43 +0200

    Cas 3 -> III

--- src/ConvertisseurNombresRomains.py
+++ src/ConvertisseurNombresRomains.py
@@ -1,4 +1,6 @@
 class ConvertisseurNombresRomains:
     @classmethod
     def convertir(cls, nombre_arabe):
-        return "I" if nombre_arabe == 1 else "II"
+        if(nombre_arabe == 1): return "I"
+        if(nombre_arabe == 2): return "II"
+        return "III"
--- test/test.py
+++ test/test.py
@@ -23,3 +23,13 @@ class NombresRomainsTest(unittest.TestCase):
 
         # ALORS on obtient "II"
         self.assertEqual("II", nombre_romain)
+
+    def test_trois(self):
+        # ETANT DONNE le chiffre 3
+        nombre_arabe = 3
+
+        # QUAND on le convertit en nombres romains
+        nombre_romain = ConvertisseurNombresRomains.convertir(nombre_arabe)
+
+        # ALORS on obtient "III"
+        self.assertEqual("III", nombre_romain)
