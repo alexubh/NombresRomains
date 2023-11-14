6aaf9bd83a2da1716591da113c1152a680a8bb42
alexubh <alexandre.ubach@ecoles-epsi.net>
2023-11-14 22:30:00 +0100

    Update __init__.py

--- test/__init__.py
+++ test/__init__.py
@@ -1,9 +1,7 @@
 import os
 import sys
-
-# Obtient le chemin du répertoire parent
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-
-# Ajoute le répertoire parent au chemin d'accès Python
-sys.path.append(parent_dir)
+PROJECT_PATH = os.getcwd()
+SOURCE_PATH = os.path.join(
+    PROJECT_PATH, "src"
+)
+sys.path.append(SOURCE_PATH)
