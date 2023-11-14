419ac15a8af3aedc05c2b090abf44096ca9737d0
alexubh <alexandre.ubach@ecoles-epsi.net>
2023-11-14 22:26:38 +0100

    test_1411_21h49

--- src/ConvertisseurNombresRomains.py
+++ src/ConvertisseurNombresRomains.py
@@ -4,3 +4,4 @@ class ConvertisseurNombresRomains:
         if(nombre_arabe == 1): return "I"
         if(nombre_arabe == 2): return "II"
         if(nombre_arabe == 3): return "III"
+# ajout commentaire inutile
